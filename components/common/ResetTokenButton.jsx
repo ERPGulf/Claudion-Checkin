e2e9f70ffb192d692844bb7cf286300a6723be1c
rishal-ERPGULF <rishal@htsqatar.com>
2025-11-20 12:04:04 +0530

    fix: update token handling in ResetTokenButton component

--- components/common/ResetTokenButton.jsx
+++ components/common/ResetTokenButton.jsx
@@ -1,17 +1,19 @@
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useDispatch } from 'react-redux';
-import { revertAll } from '../../redux/CommonActions';
- // your reset slice
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch } from "react-redux";
+import { revertAll } from "../../redux/CommonActions";
+import { saveTokens } from "../../services/api";
+// your reset slice
 
 export default function ResetTokenButton() {
   const dispatch = useDispatch();
 
   const handleResetToken = async () => {
     try {
-      await AsyncStorage.setItem('access_token',"aadasdsadsas"); // or clear all if you prefer
-    //   dispatch(revertAll());
-      console.log('Access token reset');
+      const refreshToken = await AsyncStorage.getItem("refresh_token");
+      saveTokens("aadasdsadsas", refreshToken);
+      //   dispatch(revertAll());
+      console.log("Access token reset");
     } catch (err) {
       console.log(err);
     }
@@ -26,10 +28,10 @@ export default function ResetTokenButton() {
 
 const styles = StyleSheet.create({
   button: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 40,
     right: 20,
-    backgroundColor: '#FF4C4C',
+    backgroundColor: "#FF4C4C",
     paddingVertical: 12,
     paddingHorizontal: 16,
     borderRadius: 50,
@@ -37,7 +39,7 @@ const styles = StyleSheet.create({
     zIndex: 9999,
   },
   text: {
-    color: 'white',
-    fontWeight: '600',
+    color: "white",
+    fontWeight: "600",
   },
 });
