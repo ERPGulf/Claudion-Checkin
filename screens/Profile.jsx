443bc58c4d95575a5b24a5b1fa105544c148bb54
rishal-ERPGULF <rishal@htsqatar.com>
2026-05-19 14:03:38 +0530

    chore: update version to 1.1.8 and increment build number to 6

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -425,7 +425,7 @@ function Profile() {
                   Employee account
                 </Text>
                 <Text className="mt-3 text-sm font-medium text-gray-200">
-                  Version {appVersion}-MAY-13
+                  Version {appVersion}-MAY-19
                 </Text>
 
                 <Text className="text-sm text-gray-300 mt-1">{deviceName}</Text>
