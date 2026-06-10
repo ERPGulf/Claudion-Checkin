c063430edcaca2e034db7dcb2b1ec4236aab74a8
Muhsina13 <muhsina@erpgulf.com>
2026-06-10 11:29:28 +0530

    version

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -425,7 +425,7 @@ function Profile() {
                   Employee account
                 </Text>
                 <Text className="mt-3 text-sm font-medium text-gray-200">
-                  Version {appVersion}-jun-08
+                  Version {appVersion}-jun-10
                 </Text>
 
                 <Text className="text-sm text-gray-300 mt-1">{deviceName}</Text>
